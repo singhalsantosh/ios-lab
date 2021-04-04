aa113f888968f600993d2a5fdc9cb4f6e5c795fe
Santosh Singhal <singhalsantosh@gmail.com>
2021-04-03 20:08:37 -0400

    All ios projects

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Santosh Singhal on 2/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
